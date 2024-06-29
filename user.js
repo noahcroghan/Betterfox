af030e20aa673698ba9d4e54747bb69bed1ad49c
Noah Croghan <noahcroghan0@gmail.com>
2024-06-28 22:29:04 -0500

    Add personal overrides + Smoothfox

--- user.js
+++ user.js
@@ -234,12 +234,33 @@ user_pref("layout.word_select.eat_space_to_next_word", false);
 // visit https://github.com/yokoffing/Betterfox/wiki/Optional-Hardening
 // Enter your personal overrides below this line:
 
+// Allows the theme's SVG icons to be coloured properly.
+user_pref("svg.context-properties.content.enabled", true);
+
+// Allows colours used in the theme to be mixed with others.
+user_pref("layout.css.color-mix.enabled", true);
+
+// Allows theme to use different colours for light/dark mode.
+user_pref("layout.css.light-dark.enabled", true);
+
+user_pref("browser.tabs.cardPreview.enabled", true);
 /****************************************************************************
  * SECTION: SMOOTHFOX                                                       *
 ****************************************************************************/
 // visit https://github.com/yokoffing/Betterfox/blob/main/Smoothfox.js
 // Enter your scrolling overrides below this line:
-
+user_pref("apz.overscroll.enabled", true); // DEFAULT NON-LINUX
+user_pref("general.smoothScroll", true); // DEFAULT
+user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);
+user_pref("general.smoothScroll.msdPhysics.enabled", true);
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 600);
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 650);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 25);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio", "2");
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 250);
+user_pref("general.smoothScroll.currentVelocityWeighting", "1");
+user_pref("general.smoothScroll.stopDecelerationWeighting", "1");
+user_pref("mousewheel.default.delta_multiplier_y", 300); // 250-400; adjust this number to your liking
 /****************************************************************************
  * END: BETTERFOX                                                           *
 ****************************************************************************/
